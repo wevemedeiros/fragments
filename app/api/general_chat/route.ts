e1c858f0d6b9681aac349d9894eeb140209cb77e
wevemedeiros <wguedes@remais.com.br>
2024-10-15 00:15:17 -0300

    v2

--- app/api/general_chat/route.ts
+++ app/api/general_chat/route.ts
@@ -12,7 +12,6 @@ const rateLimitMaxRequests = process.env.RATE_LIMIT_MAX_REQUESTS
   ? parseInt(process.env.RATE_LIMIT_MAX_REQUESTS)
   : 10
 
-  
 const ratelimitWindow = process.env.RATE_LIMIT_WINDOW
   ? (process.env.RATE_LIMIT_WINDOW as Duration)
   : '1d'
